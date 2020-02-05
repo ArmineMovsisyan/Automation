df8e35913aee715d551054ad36691bd1dcbcd368
ArmineMovsisyan <60317966+ArmineMovsisyan@users.noreply.github.com>
2020-02-05 14:27:25 +0400

    Update Main.java

--- src/main/java/auto_parking/Main.java
+++ src/main/java/auto_parking/Main.java
@@ -46,7 +46,7 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 if (array[i][j] != null) {
-                    System.out.print(array[i][j] + " ");
+                    System.out.print(array[i][j] + "  ");
                 }
             }
             System.out.println();
