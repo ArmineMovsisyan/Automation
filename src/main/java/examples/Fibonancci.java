0e60130609d05c52673345c68f93ba77cf70b9c2
Armine.Movsisyan <ammovsisyan95@gmail.com>
2020-02-12 16:07:03 +0400

    change examples package

--- src/main/java/examples/Fibonancci.java
+++ src/main/java/examples/Fibonancci.java
@@ -5,7 +5,7 @@ public class Fibonancci {
         fibonancii();
     }
 
-    static void fibonancii() {
+   private static void fibonancii() {
         int[] array = new int[10];
         array[0] = 1;
         array[1] = 1;
--- src/main/java/examples/Palindrome.java
+++ src/main/java/examples/Palindrome.java
@@ -1,4 +1,4 @@
-package tasks;
+package examples;
 
 import java.util.Scanner;
 
